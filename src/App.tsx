09b36a378014a10c64f05484e6ad0af1c213776a
shenxin <wb_shenxin@meituan.com>
2022-06-24 18:37:07 +0800

    feat: 引入 vue-router 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import {defineComponent, ref} from "vue";
+import {defineComponent} from "vue";
+import {RouterView} from "vue-router";
 
 export const App = defineComponent({
     setup() {
-        const refCount = ref(0);
-        const onClick = () => {
-            refCount.value += 1
-        }
         return () =>
             <>
+                <header>导航
+                    <ul>
+                        <li>
+                            <router-link to="/">foo</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/about">bar</router-link>
+                        </li>
+                    </ul>
+                </header>
                 <div>
-                    {refCount.value}
+                    <RouterView/>
                 </div>
-                <div>
-                    <button onClick={onClick}>+1</button>
-                </div>
+
             </>
     }
 })
